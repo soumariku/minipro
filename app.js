f2e08a0a3d1d2b890b91009ba47d4ecaae984dc7
unknown <unknown@example.com>
2020-04-22 00:46:25 +0800

    4.22.1
    家里传

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ App({
       wx.cloud.init({
         env:'minishop-kxw64'
       })
+      this.globalData.db = wx.cloud.database({
+        //这个是环境ID不是环境名称
+        env: 'minishop-kxw64'
+      })
     }
     // 登录
     wx.login({
@@ -27,7 +31,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              console.log(this.globalData.userInfo)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -40,6 +44,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    db:null
   }
 })
--- pages/details/details.js
+++ pages/details/details.js
@@ -1,11 +1,28 @@
+const app = getApp()
 Page({
+  onLoad:function(option){
+    this.setData({
+      goodsID: option.id
+    })
+    console.log(this.data.goodsID);
+    this.getDate();
+    this.getRule();
+  },
   data: {
     isLike: false,
     showDialog:false,
+    goodsID:'',
+    goodmsg:[],
+    flavor:[],
+    rule:[],
+    singleprice:'',
+    buysum:0,
+    pricesum:0,
+    clickflavor:'',
     // banner
     imgUrls: [
-      "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic1.jpg",
-      "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic2.jpg"
+      // "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic1.jpg",
+      // "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic2.jpg"
     ],
     indicatorDots: true, //是否显示面板指示点
     autoplay: true, //是否自动切换
@@ -14,7 +31,7 @@ Page({
 
     // 商品详情介绍
     detailImg: [
-      "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic2.jpg"
+      // "cloud://minishop-kxw64.6d69-minishop-kxw64-1301898931/pic2.jpg"
     ],
   },
   //预览图片
@@ -29,6 +46,25 @@ Page({
   toggleDialog:function(){
     this.setData({
       showDialog: !this.data.showDialog
+    });
+  },
+  tocalDialog: function () {
+    if (Number(this.data.buysum)==0){
+      this.setData({
+        showDialog: !this.data.showDialog
+      });
+    }else{
+      wx.showToast({
+        title: '添加购物车成功',
+        icon: 'success',
+        duration: 3000
+      });
+    }
+  },
+  toclickflavor(e){
+    // console.log(e.currentTarget.dataset.id)
+    this.setData({
+      clickflavor: e.currentTarget.dataset.id
     });
   },
   closeDialog:function(){
@@ -51,9 +87,92 @@ Page({
   // 立即购买
   immeBuy() {
     wx.showToast({
-      title: '购买成功',
+      title: '添加购物车成功',
       icon: 'success',
-      duration: 2000
+      duration: 3000
     });
   },
+  getDate:function() {
+    // console.log(app.globalData.db)
+    app.globalData.db.collection('goods').where({
+      _id: this.data.goodsID
+    }).get().then((res)=>{
+      let pic = [];
+      let detailspic = [];
+      pic.push(res.data[0].goodspic)
+      pic.push(res.data[0].gdetailspic)
+      detailspic.push(res.data[0].gdetailspic)
+      this.setData({
+        goodmsg: res.data[0],
+        flavor: res.data[0].flavor,
+        imgUrls: pic,
+        detailImg: detailspic
+      })
+      console.log(this.data.flavor)
+    })
+  },
+  getRule:function(){
+    app.globalData.db.collection('rule').where({
+      goodsid: this.data.goodsID
+    }).get().then((res) => {
+      console.log(res.data)
+      this.setData({
+        rule: res.data
+      })
+    })
+  },
+  //点-
+  subNum(){
+    let newsum = Number(this.data.buysum)-1
+    this.setData({
+      buysum: newsum
+    })
+    this.calculateprice()
+  },
+  //点+
+  addNum(){
+    let newsum = Number(this.data.buysum) + 1
+    this.setData({
+      buysum: newsum
+    })
+    this.calculateprice()
+  },
+  //输入购买数量
+  bindinput(e){
+    if(!e.detail.value){
+      this.setData({
+        buysum: 0
+      })
+    }else{
+      this.setData({
+        buysum: e.detail.value
+      })
+    }
+    this.calculateprice()
+  },
+  //计算价格
+  calculateprice(){
+    let newsum = 0;
+    let newsingleprice = '';
+    for(var i=0;i<this.data.rule.length;i++){
+      if (Number(this.data.rule[i].maxnum)!=0){
+        if (Number(this.data.rule[i].minnum) <= Number(this.data.buysum)){
+          if (Number(this.data.buysum)<=Number(this.data.rule[i].maxnum)){
+            newsum = Number(this.data.rule[i].price) * Number(this.data.buysum)
+            newsingleprice = this.data.rule[i].price
+            break;
+          }
+        }
+      }else{
+        newsum = Number(this.data.rule[i].price) * Number(this.data.buysum)
+        newsingleprice = this.data.rule[i].price
+        break;
+      }
+    }
+    this.setData({
+      singleprice: newsingleprice,
+      pricesum: newsum
+    })
+    // console.log(newsum)
+  }
 })
