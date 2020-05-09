d30e10bb2ca69d6f367d35a169f70334c54e17d8
unknown <unknown@example.com>
2020-05-09 17:35:29 +0800

    5.9.1
    公司

--- pages/home/home.js
+++ pages/home/home.js
@@ -112,6 +112,31 @@ Page({
       url: '../newSearch/newSearch'
     })
   },
+  //回到顶部
+  goTop: function (e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
+  // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
